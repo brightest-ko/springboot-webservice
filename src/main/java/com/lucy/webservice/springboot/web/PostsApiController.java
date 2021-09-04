99eb065f8fb040882f9617c0014f197ace6c8432
brightest-ko <brightest95hj@gmail.com>
2021-09-04 16:22:32 +0900

    #8 3.4 등록 API만들기(테스트코드포함)

---
+++ src/main/java/com/lucy/webservice/springboot/web/PostsApiController.java
@@ -0,0 +1,18 @@
+package com.lucy.webservice.springboot.web;
+
+import com.lucy.webservice.springboot.service.PostsService;
+import com.lucy.webservice.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController{
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+}
